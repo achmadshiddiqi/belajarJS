5c133b0239936c26892214d5c9ddaea2e762df1c
Shidqi <ahmadshidqi44@gmail.com>
2025-04-05 16:04:31 +0700

    update belajarObjek.js tambah pemanfaatan prototype pada object constructor

--- belajarObjek.js
+++ belajarObjek.js
@@ -31,42 +31,79 @@
 // var maba = new Mahasiswa("GORIEN", 999, "Namek");
 
 // 4. Object.create()
-let AktivitasHP = {
-  browsing: function (jam) {
-    this.baterai -= jam * 3;
-    if (this.baterai < 0) {
-      this.baterai = 0;
-      return console.log(`Baterai sudah habis!!`);
-    } else if (this.baterai > 0) {
-      return console.log(`Browsing menghabiskan baterai ${jam * 3}%`);
-    }
-  },
+// let AktivitasHP = {
+//   browsing: function (jam) {
+//     this.baterai -= jam * 3;
+//     if (this.baterai < 0) {
+//       this.baterai = 0;
+//       return console.log(`Baterai sudah habis!!`);
+//     } else if (this.baterai > 0) {
+//       return console.log(`Browsing menghabiskan baterai ${jam * 3}%`);
+//     }
+//   },
 
-  gaming: function (jam) {
-    this.baterai -= jam * 5;
-    if (this.baterai < 0) {
-      this.baterai = 0;
-      return console.log(`Baterai sudah habis!!`);
-    } else if (this.baterai > 0) {
-      return console.log(`Bermain game menghabiskan baterai ${jam * 5}%`);
-    }
-  },
+//   gaming: function (jam) {
+//     this.baterai -= jam * 5;
+//     if (this.baterai < 0) {
+//       this.baterai = 0;
+//       return console.log(`Baterai sudah habis!!`);
+//     } else if (this.baterai > 0) {
+//       return console.log(`Bermain game menghabiskan baterai ${jam * 5}%`);
+//     }
+//   },
 
-  charge: function (jam) {
-    this.baterai += jam * 50;
-    if (this.baterai > 100) {
-      this.baterai = 100;
-      return console.log(`Baterai telah terisi penuh!!`);
-    } else {
-      return console.log(`Baterai telah terisi ${jam * 50}%`);
-    }
-  },
-};
+//   charge: function (jam) {
+//     this.baterai += jam * 50;
+//     if (this.baterai > 100) {
+//       this.baterai = 100;
+//       return console.log(`Baterai telah terisi penuh!!`);
+//     } else {
+//       return console.log(`Baterai telah terisi ${jam * 50}%`);
+//     }
+//   },
+// };
 
+// function HP(baterai) {
+//   let Hape = Object.create(AktivitasHP);
+//   Hape.baterai = baterai;
+//   return Hape;
+// }
+
+// let Rina = HP(100);
+
+// 5. Constructor dengan memanfaatkan prototype
 function HP(baterai) {
-  let Hape = Object.create(AktivitasHP);
-  Hape.baterai = baterai;
-  return Hape;
+  this.baterai = baterai;
 }
 
-let Rina = HP(100);
+HP.prototype.browsing = function (jam) {
+  this.baterai -= jam * 3;
+  if (this.baterai < 0) {
+    this.baterai = 0;
+    return console.log(`Baterai sudah habis!!`);
+  } else if (this.baterai > 0) {
+    return console.log(`Browsing menghabiskan baterai ${jam * 3}%`);
+  }
+};
+
+HP.prototype.gaming = function (jam) {
+  this.baterai -= jam * 5;
+  if (this.baterai < 0) {
+    this.baterai = 0;
+    return console.log(`Baterai sudah habis!!`);
+  } else if (this.baterai > 0) {
+    return console.log(`Bermain game menghabiskan baterai ${jam * 5}%`);
+  }
+};
+
+HP.prototype.charge = function (jam) {
+  this.baterai += jam * 50;
+  if (this.baterai > 100) {
+    this.baterai = 100;
+    return console.log(`Baterai telah terisi penuh!!`);
+  } else {
+    return console.log(`Baterai telah terisi ${jam * 50}%`);
+  }
+};
+
+let Rina = new HP(100);
