882d0779d89948a17141a9ea7ad0c27f0c45d626
Shidqi <ahmadshidqi44@gmail.com>
2025-04-15 08:41:52 +0700

    tambah file pseudo-code latihan Filter, Map, dan Reduce

---
+++ latihanFilterMapReduce.js
@@ -0,0 +1,13 @@
+// Ambil semua elemen video
+
+// Pilih video "JAVASCRIPT LANJUTAN"
+
+// Ambil durasi masing-masing video
+
+// Ubah durasi dari str menjadi int, ubah satuan menit menjadi detik
+
+// Jumlahkan durasi detik
+
+// Ubah formatnya jadi jam-menit-detik
+
+// Simpan di DOM
