85fd9a2b64037a04d43f8a36aa0bea9a79d16d75
Shidqi <ahmadshidqi44@gmail.com>
2025-05-06 09:05:11 +0700

    revisi cara ganti src pada image gallery

--- latihanImageGallery.js
+++ latihanImageGallery.js
@@ -3,8 +3,7 @@ const gBesar = document.querySelector(".gambar-besar img");
 const thumbnail = document.querySelector(".thumbnail");
 
 thumbnail.addEventListener("click", (e) => {
-  getThumbAttribute = e.target.getAttribute("src");
   if (e.target.className == "thumb") {
-    gBesar.setAttribute("src", getThumbAttribute);
+    gBesar.src = e.target.src;
   }
 });
