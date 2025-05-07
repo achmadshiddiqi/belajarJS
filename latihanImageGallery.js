70c9caad33ac6dc9aea8cf1605cb055be9eb639f
Shidqi <ahmadshidqi44@gmail.com>
2025-05-07 08:08:21 +0700

    add efek opacity permanen saat thumbnail diklik

--- latihanImageGallery.js
+++ latihanImageGallery.js
@@ -1,6 +1,7 @@
 // Method ubah gambar besar sesuai thumbnail yang diklik
 const gBesar = document.querySelector(".gambar-besar img");
 const thumbnail = document.querySelector(".thumbnail");
+const thumbs = document.querySelectorAll(".thumb");
 
 thumbnail.addEventListener("click", (e) => {
   if (e.target.className == "thumb") {
@@ -9,5 +10,11 @@ thumbnail.addEventListener("click", (e) => {
     setTimeout(() => {
       gBesar.classList.remove("fade");
     }, 500);
+
+    thumbs.forEach((thumb) => {
+      thumb.className = "thumb";
+    });
+
+    e.target.classList.add("active");
   }
 });
